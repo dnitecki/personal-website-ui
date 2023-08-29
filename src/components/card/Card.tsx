cbb5ddfd94b2a7411ddcdc4a6e57df837d835f64
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2023-08-29 17:13:51 -0400

    add card css

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Card.scss";
 
 export default function Card() {
-  return <div>Card</div>;
+  return (
+    <div className="glass-card">
+      <h2>Card</h2>
+    </div>
+  );
 }
