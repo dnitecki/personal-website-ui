5d42ff66432fd86c762a651b129bef030b86a10f
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2023-08-29 15:33:05 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.scss";
 import Background from "./components/background/Background";
+import MainPage from "./pages/main/MainPage";
 
 function App() {
   return (
     <div className="app">
       <Background />
+      <div className="app-container">
+        <MainPage />
+      </div>
     </div>
   );
 }
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./MainPage.scss";
+
+export default function MainPage() {
+  return <div>MainPage</div>;
+}
