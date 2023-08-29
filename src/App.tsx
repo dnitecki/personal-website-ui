27b6308685ac9b3ff049ca2427ad4bdc8861fb03
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2023-08-29 15:01:30 -0400

    background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.scss";
-import BackgroundLight from "./components/backgroundLight/BackgroundLight";
+import Background from "./components/background/Background";
 
 function App() {
   return (
     <div className="app">
-      <BackgroundLight />
+      <Background />
     </div>
   );
 }
