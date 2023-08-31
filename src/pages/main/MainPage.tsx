fdfc89465eaf3c0a2b78d4e9c4bf60551a2d4e60
dnitecki <dominick.nitecki@outlook.com>
2023-08-30 20:09:32 -0400

    add animations

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./MainPage.scss";
 import Card from "../../components/card/Card";
+import Lottie from "lottie-react";
+import Logo from "./MyLogo.json";
 
 export default function MainPage() {
   return (
     <div className="main-page-content">
       <Card />
+      <div>
+        <Lottie animationData={Logo} />
+      </div>
     </div>
   );
 }
