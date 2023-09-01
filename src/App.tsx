73cfae050df9e2969febfa1434635584bdc7aad7
dnitecki <dominick.nitecki@outlook.com>
2023-08-31 23:13:51 -0400

    fix scroll issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,6 @@ import Background from "./components/background/Background";
 import MainPage from "./pages/main/MainPage";
 
 function App() {
-  window.addEventListener("load", function () {
-    setTimeout(function () {
-      // This hides the address bar:
-      window.scrollTo(0, 1);
-    }, 0);
-  });
-
-  useEffect(() => {
-    if (window.scrollY >= 100) {
-      window.scrollTo(0, 1);
-    } else {
-      window.scrollTo(0, 0);
-    }
-  }, [window.scrollY]);
-
   return (
     <div className="app">
       <Background />
