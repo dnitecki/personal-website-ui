f9049a56576296005c4e1810f638f6ae29e9a62e
dnitecki <dominick.nitecki@outlook.com>
2023-09-02 17:54:46 -0400

    add slider

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./MainPage.scss";
 import Card from "../../components/card/Card";
 import Lottie from "react-lottie-player";
 import Logo from "./MyLogo.json";
 import BottomNav from "../../components/bottomNav/BottomNav";
+import { motion } from "framer-motion";
 
 const JPMCLogo = require("../../assets/JP-Morgan-Chase-Logo.png");
 const STNLogo = require("../../assets/Stantec-Logo.png");
 
 export default function MainPage() {
+  const [width, setWidth] = useState(0);
+  const slider = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    setWidth(slider.current.scrollWidth - slider.current.offsetWidth);
+  }, []);
   return (
     <div className="main-page-content">
       <div className="main-page-logo">
         <Lottie animationData={Logo} className="player" loop play />
       </div>
       <div className="resume">
-        <div className="glass resume-card">
-          <div className="top-card">
-            <div className="logo-container">
-              <img className="JPMCLogo" src={JPMCLogo} alt="logo" />
-            </div>
-          </div>
-        </div>
-        <div className="glass resume-card">
-          <div className="top-card">
-            <div className="logo-container">
-              <img className="STNLogo" src={STNLogo} alt="logo" />
-            </div>
-          </div>
-        </div>
+        <motion.div ref={slider} className="slider">
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            className="inner-slider"
+          >
+            <motion.div className="item">
+              <div className="glass resume-card">
+                <div className="top-card">
+                  <div className="logo-container">
+                    <img className="JPMCLogo" src={JPMCLogo} alt="logo" />
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+            <motion.div className="item">
+              <div className="glass resume-card">
+                <div className="top-card">
+                  <div className="logo-container">
+                    <img className="STNLogo" src={STNLogo} alt="logo" />
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
       </div>
-      <Card />
-      <Card />
       <Card />
       <Card />
       <Card />
