5ba1e143f6e3020203da30c0724283ee66e67a06
dnitecki <dominick.nitecki@outlook.com>
2023-09-01 21:39:31 -0400

    add resume cards

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -5,11 +5,30 @@ import Lottie from "react-lottie-player";
 import Logo from "./MyLogo.json";
 import BottomNav from "../../components/bottomNav/BottomNav";
 
+const JPMCLogo = require("../../assets/JP-Morgan-Chase-Logo.png");
+const STNLogo = require("../../assets/Stantec-Logo.png");
+
 export default function MainPage() {
   return (
     <div className="main-page-content">
       <div className="main-page-logo">
         <Lottie animationData={Logo} className="player" loop play />
+      </div>
+      <div className="resume">
+        <div className="glass resume-card">
+          <div className="top-card">
+            <div className="logo-container">
+              <img className="JPMCLogo" src={JPMCLogo} alt="logo" />
+            </div>
+          </div>
+        </div>
+        <div className="glass resume-card">
+          <div className="top-card">
+            <div className="logo-container">
+              <img className="STNLogo" src={STNLogo} alt="logo" />
+            </div>
+          </div>
+        </div>
       </div>
       <Card />
       <Card />
