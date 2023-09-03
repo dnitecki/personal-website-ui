8790b0370084a8531b8c2e43aabc4c08308310e8
dnitecki <dominick.nitecki@outlook.com>
2023-09-02 23:14:53 -0400

    commit

--- src/components/resumeSlider/ResumeSlider.tsx
+++ src/components/resumeSlider/ResumeSlider.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./ResumeSlider.scss";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const JPMCLogo = require("../../assets/JP-Morgan-Chase-Logo.png");
 const STNLogo = require("../../assets/Stantec-Logo.png");
@@ -13,33 +13,48 @@ export default function ResumeSlider() {
   }, []);
   return (
     <div>
-      <motion.div ref={slider} className="slider">
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          animate={{ x: 0 }}
-          className="inner-slider"
-        >
-          <motion.div className="item">
-            <div className="glass resume-card">
-              <div className="top-card">
-                <div className="logo-container">
-                  <img className="JPMCLogo" src={JPMCLogo} alt="logo" />
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div ref={slider} className="slider">
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            dragPropagation
+            style={{ touchAction: "none" }}
+            className="inner-slider"
+            animate={{
+              x: 0,
+            }}
+          >
+            <motion.div className="item">
+              <div className="glass resume-card">
+                <div className="top-card">
+                  <div className="logo-container">
+                    <img className="JPMCLogo" src={JPMCLogo} alt="logo" />
+                  </div>
                 </div>
               </div>
-            </div>
-          </motion.div>
-          <motion.div className="item">
-            <div className="glass resume-card">
-              <div className="top-card">
-                <div className="logo-container">
-                  <img className="STNLogo" src={STNLogo} alt="logo" />
+            </motion.div>
+            <motion.div className="item">
+              <div className="glass resume-card">
+                <div className="top-card">
+                  <div className="logo-container">
+                    <img className="STNLogo" src={STNLogo} alt="logo" />
+                  </div>
                 </div>
               </div>
-            </div>
+            </motion.div>
+            <motion.div className="item">
+              <div className="glass resume-card">
+                <div className="top-card">
+                  <div className="logo-container">
+                    <img className="STNLogo" src={STNLogo} alt="logo" />
+                  </div>
+                </div>
+              </div>
+            </motion.div>
           </motion.div>
         </motion.div>
-      </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
