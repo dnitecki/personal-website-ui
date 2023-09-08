721968e3ce778332dda66dbd2dc98bcb16bf37e3
dnitecki <dominick.nitecki@outlook.com>
2023-09-07 20:45:25 -0400

    css changes

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -11,8 +11,11 @@ export default function Resume() {
           <h2>Add Info</h2>
         </div>
         <div className="resume-card-back">
+          <div className="resume-firm">
+            <h1>JPMorgan Chase</h1>
+          </div>
           <div className="resume-title">
-            <h2>JPMorgan Chase</h2>
+            <h2>Software Engineer</h2>
           </div>
           <div className="resume-dates">
             <h3>Nov 2022 to Present</h3>
@@ -26,8 +29,11 @@ export default function Resume() {
           <h2>Add Info</h2>
         </div>
         <div className="resume-card-back">
+          <div className="resume-firm">
+            <h1>Stantec</h1>
+          </div>
           <div className="resume-title">
-            <h2>Stantec</h2>
+            <h2>Application Developer</h2>
           </div>
           <div className="resume-dates">
             <h3>Jan 2021 to Nov 2022</h3>
