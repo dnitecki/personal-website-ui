72ea5f534286f7ecc3d0c546d89f74c5388d3ace
dnitecki <dominick.nitecki@outlook.com>
2023-09-17 00:54:18 -0400

    toggle

--- src/components/modeToggle/ModeToggle.tsx
+++ src/components/modeToggle/ModeToggle.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./ModeToggle.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export default function ModeToggle() {
   return (
     <div className="mode-toggle-container">
       <input type="checkbox" id="mode-toggle" className="mode-toggle" />
-      <label htmlFor="mode-toggle" className="toggle-label"></label>
+      <label htmlFor="mode-toggle" className="toggle-label">
+        <div className="toggle-icons">
+          <FontAwesomeIcon className="moon" icon={faMoon} />
+          <FontAwesomeIcon className="sun" icon={faSun} />
+        </div>
+      </label>
     </div>
   );
 }
