8e61eb600fade57a20dcd15e083b1370a9fdb022
dnitecki <dominick.nitecki@outlook.com>
2023-09-26 21:12:41 -0400

    update logos

--- src/components/certs/Certs.tsx
+++ src/components/certs/Certs.tsx
@@ -3,10 +3,18 @@ import "./Certs.scss";
 import cloudPractitioner from "../../assets/AWSCP.png";
 import ibmCert from "../../assets/IBMPC.png";
 import ibmPython from "../../assets/IBMPY.png";
+import associateDeveloper from "../../assets/AWSDA.png";
 
 export default function Certs() {
   return (
     <div className="cert-container">
+      <div className="glass cert">
+        <img
+          className="cert-icon"
+          src={associateDeveloper}
+          alt="AWS Developer Associate"
+        />
+      </div>
       <div className="glass cert">
         <img
           className="cert-icon"
@@ -15,14 +23,14 @@ export default function Certs() {
         />
       </div>
       <div className="glass cert">
-        <img className="cert-icon" src={ibmCert} alt="AWS Cloud Practitioner" />
-      </div>
-      <div className="glass cert">
         <img
           className="cert-icon"
-          src={ibmPython}
-          alt="AWS Cloud Practitioner"
+          src={ibmCert}
+          alt="IBM Data Science Professional"
         />
+      </div>
+      <div className="glass cert">
+        <img className="cert-icon" src={ibmPython} alt="IBM Python" />
       </div>
     </div>
   );
