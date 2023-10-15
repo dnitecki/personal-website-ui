d932306f90bcb03aa213c41a613dc5eb7f429548
dnitecki <dominick.nitecki@outlook.com>
2023-10-14 21:24:40 -0400

    commit changes

--- src/components/portfolioSlider/PortfolioSlider.tsx
+++ src/components/portfolioSlider/PortfolioSlider.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./PortfolioSlider.scss";
 import { motion } from "framer-motion";
 import finfetch from "../../assets/FinfetchMockup.png";
@@ -7,6 +8,7 @@ import insurance from "../../assets/InsuranceMockup.png";
 import insuranceIcon from "../../assets/onestop-icon.png";
 
 export default function PortfolioSlider() {
+  let navigate = useNavigate();
   const [width, setWidth] = useState(0);
   const slider = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
