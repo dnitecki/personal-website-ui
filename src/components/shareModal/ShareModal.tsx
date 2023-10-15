46a5d0e8a9e3e885e8c0cc8c57934c9174a31522
dnitecki <dominick.nitecki@outlook.com>
2023-10-15 13:46:04 -0400

    add speedbump

--- src/components/shareModal/ShareModal.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./ShareModal.scss";
-
-export default function ShareModal() {
-  return <div>ShareModal</div>;
-}
---
+++ src/components/speedBump/SpeedBump.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SpeedBump.scss";
+
+export default function SpeedBump() {
+  return <div>SpeedBump</div>;
+}
