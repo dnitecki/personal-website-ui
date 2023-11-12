3a89a1a352fe753b5d2b3f7c6d8e7a5aba0bb9e5
dnitecki <dominick.nitecki@outlook.com>
2023-11-11 22:37:58 -0500

    animation

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -12,7 +12,14 @@ export default function MainPage() {
       <div id="main-page-content" className="main-page-content">
         <ModeToggle />
         {MainPageItems.map((section: MainPageType, index: number) => (
-          <motion.div key={index} id={section.id} className={section.className}>
+          <motion.div
+            key={index}
+            id={section.id}
+            className={section.className}
+            initial={{ translateY: 200 }}
+            animate={{ translateY: 0 }}
+            transition={{ duration: 1, delay: 3.5 + index * 0.2 }}
+          >
             {section.headerText && (
               <div className="section-name">
                 <h1>{section.headerText}</h1>
