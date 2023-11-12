032dbed49b03ee2683b30021e5c3d329d7f1f520
dnitecki <dominick.nitecki@outlook.com>
2023-11-11 23:41:00 -0500

    change syntax

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -2,19 +2,19 @@ import React, { useEffect } from "react";
 import "./Background.scss";
 
 export default function Background() {
-  function createStars(stars: number) {
+  const createStars = (stars: number) => {
     for (let i = 0; i < stars; i++) {
       drawStar();
     }
-  }
+  };
 
-  function drawStar() {
+  const drawStar = () => {
     const tmpStar = document.createElement("figure");
     tmpStar.className = "star";
     tmpStar.style.top = 100 * Math.random() + "%";
     tmpStar.style.left = 100 * Math.random() + "%";
     document.getElementById("stars").appendChild(tmpStar);
-  }
+  };
 
   useEffect(() => {
     createStars(75);
