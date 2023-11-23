1ee30b12e9a1380c3e182d90b3c618a6866c8784
dnitecki <dominick.nitecki@outlook.com>
2023-11-23 18:36:42 -0500

    add success animation

--- src/components/envelope/Envelope.tsx
+++ src/components/envelope/Envelope.tsx
@@ -12,6 +12,27 @@ export default function Envelope() {
 
   return (
     <div className="envelope-container">
+      <div className="sent-text">
+        <svg
+          className="checkmark"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 52 52"
+        >
+          <circle
+            className="checkmark__circle"
+            cx="26"
+            cy="26"
+            r="25"
+            fill="none"
+          />
+          <path
+            className="checkmark__check"
+            fill="none"
+            d="M14.1 27.2l7.1 7.2 16.7-16.8"
+          />
+        </svg>
+        <h1>Message Sent!</h1>
+      </div>
       <div className={`envelope ${formComplete ? "close" : "open"}`}>
         <div className="front-envelope flap"></div>
         <div className="front-envelope pocket"></div>
