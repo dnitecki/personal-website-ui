c0f3697c45b04c6a248236f81757714706f70655
dnitecki <dominick.nitecki@outlook.com>
2024-10-22 20:46:01 -0400

    refactor app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { ParallaxProvider } from "react-scroll-parallax";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>
-  </React.StrictMode>
+  <ParallaxProvider>
+    <App />
+  </ParallaxProvider>
 );
