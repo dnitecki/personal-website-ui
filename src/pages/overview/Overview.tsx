2dde3a35c9128b7571d7a356ae598c7e97e528d2
dnitecki <dominick.nitecki@outlook.com>
2024-10-27 18:47:24 -0400

    add header back

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -1,6 +1,11 @@
+import Header from "../../components/header/Header";
 import "./Overview.scss";
 const Overview = () => {
-  return <div className="overview-container">Overview</div>;
+  return (
+    <div className="overview-container">
+      <Header />
+    </div>
+  );
 };
 
 export default Overview;
