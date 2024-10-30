ad83dfa9338f3ab2fd53a5a291526f7587715d10
dnitecki <dominick.nitecki@outlook.com>
2024-10-29 23:40:09 -0400

    outline overview page

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -2,7 +2,12 @@ import "./Overview.scss";
 const Overview = () => {
   return (
     <div className="overview-container">
-      <h1>Overview</h1>
+      <div className="overview-grid">
+        <div className="aboutme-container"></div>
+        <div className="portfolio-container"></div>
+        <div className="photo-container"></div>
+        <div className="certs-container"></div>
+      </div>
     </div>
   );
 };
