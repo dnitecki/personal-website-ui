9398e763a0011563b22f935906783502b97d9ad1
dnitecki <dominick.nitecki@outlook.com>
2024-11-03 01:13:32 -0500

    adjust timeline styles

--- src/components/customTimeline/CustomTimeline.tsx
+++ src/components/customTimeline/CustomTimeline.tsx
@@ -6,24 +6,20 @@ import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent/TimelineOppositeContent";
-import { COLORS } from "../../constants/constants";
 import { TimelineItems } from "../../mappers/timelineMapper";
 
 const CustomTimeline = () => {
   return (
     <div className="timeline-container">
-      <Timeline position="alternate">
+      <Timeline>
         {TimelineItems.map((item, index) => (
           <TimelineItem key={index}>
-            <TimelineOppositeContent>
-              <div
-                className={`timeline-opposite-content ${
-                  index % 2 === 0 && "even"
-                }`}
-              >
+            <TimelineOppositeContent sx={{ width: "10rem" }}>
+              <div className="timeline-opposite-content">
                 <h2>{item.timeFrame}</h2>
               </div>
             </TimelineOppositeContent>
+
             <TimelineSeparator>
               <TimelineConnector />
               <TimelineDot sx={{ bgcolor: "transparent" }}>
@@ -40,9 +36,7 @@ const CustomTimeline = () => {
                     <div className="timeline-header">
                       <h2>{item.position}</h2>
                       <h3>{item.companyName}</h3>
-                      <div
-                        className={`timeline-chips ${index % 2 != 0 && "odd"}`}
-                      >
+                      <div className="timeline-chips">
                         {item.skills.map((skill, index) => (
                           <div className="chip" key={index}>
                             <p>{skill.text}</p>
