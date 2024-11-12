d19a1b013c045b8c481882f1172b41b44efc9f0f
dnitecki <dominick.nitecki@outlook.com>
2024-11-12 18:46:38 -0500

    css changes

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -5,7 +5,6 @@ import "./Resume.scss";
 const Resume = () => {
   return (
     <div className="resume-container">
-      <h1>Resume</h1>
       <div className="resume-content">
         <CustomTimeline />
         <Certifications />
